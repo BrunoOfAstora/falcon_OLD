8586c191ca1e71096a1d48a9be3ab2770c37161e
BrunoOfAstora <bruno.cntt@hotmail.com>
2025-06-01 17:17:19 -0300

    just comment

--- source/flcn-main.c
+++ source/flcn-main.c
@@ -3,12 +3,12 @@
 #include "flcn-command.h"
 #include <stdio.h>
 #include <string.h>
-
-/* flc-main.c */
+#include <stdlib.h>
+						/* flc-main.c */
 
 void print_help()
 {
-printf("\n>>Help Menu\n\n Usage => falcon <option> <file>\n\n Functions:\n  sha256 \t Calculates the SHA256 of the file\n  md5 \t\t Return the hash MD5 of the file\n  \n\n");
+printf("\n>>Help Menu\n\n Usage => falcon <option> <file>\n\n Functions:\n  sha256 \t Calculates the SHA256 of the file\n  md5 \t\t Return the hash MD5 of the file\n More Functions will be added soon \n\n");
 return;
 }
 
@@ -16,8 +16,9 @@ return;
 int main(int argc, char *argv[])
 {
 
-     if(argc < 2){ printf("\nERROR : Less than one argument\n\n\tUsage => falcon <option> <file> \n\n"); return 0; }
-	
+    if(argc < 2){ printf("\nERROR : Less than one argument\n\n\tUsage => falcon <option> <file> \n\n"); return 0; }
+  	
+    char *hash_md5; 	
 
     char *userFileInput = argv[2];
     char *options = argv[1];
@@ -27,12 +28,12 @@ int main(int argc, char *argv[])
     
     switch(cmd)
     {
-      case CMD_HELP:
+    case CMD_HELP:
 	print_help();
 	break;
 
 
-      case CMD_SHA256: //SHA256
+    case CMD_SHA256: //SHA256
 	if(argc != 3)
 	{
 	  printf("\n\n The sha256 command should receive one and only one argument(file) \n\n");
@@ -42,17 +43,19 @@ int main(int argc, char *argv[])
 	break;
 
 
-      case CMD_MD5:   //MD5
+    case CMD_MD5:   //MD5
 	if(argc != 3)
 	{
 	 printf("\n\n The MD5 command should receive one and only one argument(file)");
 	 break;
 	}
-	calculate_md5(userFileInput);
+	hash_md5 = calculate_md5(userFileInput);
+    printf("\n\nMD5: %s\n\n", hash_md5);
+    free(hash_md5);
 	break;
 
 
-      default:
+    default:
 	printf("invalid command, try 'falcon -h' for help\n");
 	break;
     }
