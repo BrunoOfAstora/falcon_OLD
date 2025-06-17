efee95fdebbdd95982d00a06f4c49179c4560052
BrunoOfAstora <bruno.cntt@hotmail.com>
2025-06-17 12:55:15 -0300

    [CHECKPOINT]:checkpoint before modify verify function

--- source/flcn-verify.c
+++ source/flcn-verify.c
@@ -2,57 +2,106 @@
 
 #include "flcn-verify.h"
 #include "flcn-sha384.h"
+
+#include <syss/stat.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 128
+#define MAX 512
 
-void verify_hash(const char *userFileInput, char *savedFilePath)
-{    						// OK! \\ remaking the function in the right way.(i think...); 
+struct Tokens
+{
+	char *nameTok;
+	char *hashTok;
+};
 
-char *str_buffer[2]; //The new string will store the copy of file name and hash of "savedPathFile"
-str_buffer[0] = malloc(MAX);;
-str_buffer[1] = malloc(MAX);
 
-int i, j;	//iterators
-i = j = 0;  //i
+void verify_all(const char *userFileInput, char *savedFilePath)
+{
+	
 
-char line_buffer[MAX], 
-	 *result,
-	 *token, 
-	 *state, 
-	 *sha384_result;
+}
 
-FILE *comparasion_file;
-FILE *original_file;
 
-sha384_result = calculate_hash_384(userFileInput);
+void verify_hash(const char *userFileInput, char *savedFilePath) //<- Need to bee removed after, or at least modified, since this function will check a default dir
+{                                        
+    char *str_buffer[2]; //The new string will store the copy of file name and hash of "savedPathFile"
+    str_buffer[0] = malloc(MAX);
+    str_buffer[1] = malloc(MAX);
 
-original_file = fopen(userFileInput, "r");
-if(!original_file) { perror("\nerror in verify function while opening userFileInput\n"); return; }
+    int i, j;    //iterators
+    i = j = 0;   //i
 
-comparasion_file = fopen(savedFilePath, "r");
-if(!comparasion_file) { perror("\nerror in verify function while opening the savedFilePath\n"); return; }
+    char line_buffer[MAX],
+         *result,
+         *token,
+         *state,
+         *sha384_result;
 
-if((result = fgets(line_buffer, MAX, comparasion_file)) != NULL)
-	token = strtok_r(result, "/", &state); //Check the strtok_r documentation
+    FILE *comparasion_file;
+    FILE *original_file;
 
-while(token != NULL && i <= 2)
- {
-  strcpy(str_buffer[i], token);
-  token = strtok_r(NULL, "/", &state);
-  i++;
- }
+    sha384_result = calculate_hash_384(userFileInput);
 
-str_buffer[1][strcspn(str_buffer[1], "\n")] = '\0';
+    original_file = fopen(userFileInput, "r");
 
-if((strcmp(str_buffer[0], userFileInput)) == 0) { printf("\n\033[1;32m[OK!]\033[0m File Name match.\n"); }
-	else{ printf("\n\033[1;33m[Warning]\033[0m File Name Does Not Match!\n"); }
+    if (!original_file) 
+	{
+        perror("\nerror in verify function while opening userFileInput\n");
+        return;
+    }
 
-if((strcmp(str_buffer[1], sha384_result)) == 0) { printf("\n\033[1;32m[OK!]\033[0m Hash Checksum match.\n"); }
-	else{ printf("\n\033[1;33m[CAUTION]\033[0m Checksum Does Not Mactch!"); }
+    comparasion_file = fopen(savedFilePath, "r");
 
-return;
+    if (!comparasion_file) 
+	{
+        perror("\nerror in verify function while opening the savedFilePath\n");
+        return;
+    }
 
-}                                                  
+    if ((result = fgets(line_buffer, MAX, comparasion_file)) != NULL)
+        token = strtok_r(result, "/", &state); //Check the strtok_r documentation
+
+    while (token != NULL && i <= 1) 
+	{
+		if(strlen(token) >= MAX)
+		{
+			printf("\nBuffer Overflow Avoided. The string is too long.\n");
+			return;
+		}
+
+        strcpy(str_buffer[i], token);
+        token = strtok_r(NULL, "/", &state);
+        i++;
+    }
+
+    str_buffer[1][strcspn(str_buffer[1], "\n")] = '\0';
+
+    if ((strcmp(str_buffer[0], userFileInput) == 0 && strcmp(str_buffer[1], sha384_result)) == 0) 
+	{
+        printf("\n\033[1;32m[OK!]\033[0m File Name match.\n");
+        printf("\n\033[1;32m[OK!]\033[0m Hash Checksum match.\n");
+    } 
+	
+	else if ((strcmp(str_buffer[0], userFileInput) == 0 && strcmp(str_buffer[1], sha384_result)) != 0) 
+	{
+        printf("\n\033[1;32m[OK!]\033[0m File Name match.\n");
+        printf("\n\033[1;33m[Warning]\033[0m Checksum Does Not Match!\n");
+    }
+
+    if ((strcmp(str_buffer[1], sha384_result)) == 0) 
+	{
+        printf("\n\033[1;32m[OK!]\033[0m Hash Checksum match.\n");
+    }
+
+	 else
+	 {
+        printf("\n\033[1;33m[CAUTION]\033[0m Checksum Does Not Mactch!");
+     }
+
+    return;
+}                                            
